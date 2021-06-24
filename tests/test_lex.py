ef4e28accb6fa31152373b2aabb4e1be985d27a3
Armani Tallam <armanitallam@gmail.com>
2021-06-24 16:28:52 +0300

    Add tests for the first part of the lexer.
    `to_utf8` hasn't been implemented yet, but it will be a function that takes `bytes` and converts them to a UTF-8 `str`.

---
+++ tests/test_lex.py
@@ -0,0 +1,38 @@
+# pylint: disable=C0116, E1101
+from pytest import main, mark, raises
+
+from hasdrubal.lex import to_utf8
+
+
+@mark.lexer
+@mark.parametrize(
+    "source,expected",
+    (
+        (b"", ""),
+        (b"English", "English"),
+        (b"Fran\xc3\xa7ais", "Français"),
+        (b"ma\xc3\xb1ana ol\xc3\xa9", "mañana olé"),
+        (b"\xcf\x89\xcf\x81\xce\xaf", "ωρί"),
+        (b"\xd0\x94\xd0\xb5\xd1\x81\xd1\x8f\xd1\x82", "Десят"),
+        (b"\xe3\x83\xa6\xe3\x82\xb6\xe3\x83\xbc\xe5\x88\xa5\xe3\x82\xb5", "ユザー別サ"),
+    ),
+)
+def test_to_utf8_without_errors(source, expected):
+    assert to_utf8(source) == expected
+
+
+@mark.lexer
+@mark.parametrize(
+    "source",
+    (
+        b"\xcf\x89\xcf\x81\xcf",
+        b"\xe3\x83\xa6\xe3\x82\xb6\xe3\x83\xbc\xe5\x88\xa5\xe3\x82",
+    ),
+)
+def test_to_utf8_with_errors(source):
+    with raises(UnicodeDecodeError):
+        to_utf8(source)
+
+
+if __name__ == "__main__":
+    main()
