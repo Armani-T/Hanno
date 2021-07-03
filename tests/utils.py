bdc152198f71774af1a04a7381055281ae697d86
Armani Tallam <armanitallam@gmail.com>
2021-07-03 22:45:20 +0300

    Add tests for the new responsibility that `build_token` has

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,22 @@
 # pylint: disable=W0612
+from typing import Tuple
 
 SAMPLE_SOURCE = "let l = [1, 2, 3] <> [4, 5, 6] in head(l)"
 SAMPLE_SOURCE_PATH = __file__
+
+
+class FakeMatch:
+    """This class is used to mock passing a `re.Match` object."""
+
+    def __init__(self, span: Tuple[int, int], lastgroup: str, text: str) -> None:
+        self._span = span
+        self.lastgroup = lastgroup
+        self.text = text
+
+    def span(self):
+        return self._span
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.text
+        raise RuntimeError("`FakeMatch` can only index the value 0.")
