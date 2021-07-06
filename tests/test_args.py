133fcc2cd9c8a1edb032692ab8ab2f931e8f054a
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-07-06 18:12:35 +0300

    Add tests for `build_config`
    This will increase testing coverage and force the program to have sane
    defaults for flags passed in from the command line.

---
+++ tests/test_args.py
@@ -0,0 +1,46 @@
+# pylint: disable=C0116, W0612
+from codecs import lookup
+
+from pytest import mark
+
+from context import args
+
+
+class FakeNamespace:
+    pass
+
+
+defaults = {
+    "file": None,
+    "source_encoding": "utf-8",
+    "show_help": False,
+    "show_version": False,
+    "show_tokens": False,
+}
+
+
+@mark.cmd
+@mark.parametrize(
+    "cmd_args,expected",
+    (
+        ((), {}),
+        (("-?",), {"show_help": True}),
+        (
+            ("--lex", "-e", "utf16"),
+            {"source_encoding": "utf16", "show_tokens": True},
+        ),
+    ),
+)
+def test_build_config(cmd_args, expected):
+    namespace = FakeNamespace()
+    args.parser.parse_args(args=cmd_args, namespace=namespace)
+    config = args.build_config(namespace)
+    actual_expected = {**defaults, **expected}
+
+    assert callable(config.report_error)
+    assert callable(config.write)
+    assert lookup(config.source_encoding) == lookup(actual_expected["source_encoding"])
+    assert config.file == actual_expected["file"]
+    assert config.show_help == actual_expected["show_help"]
+    assert config.show_version == actual_expected["show_version"]
+    assert config.show_tokens == actual_expected["show_tokens"]
