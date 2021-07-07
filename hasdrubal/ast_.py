bb9872b6bb1afc5712a332d428fda93097fe1b25
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-07-07 11:16:16 +0300

    Add the AST nodes that the parser will generate

---
+++ hasdrubal/ast_.py
@@ -0,0 +1,277 @@
+# pylint: disable=R0903
+from abc import ABC, abstractmethod
+from enum import auto, Enum
+from typing import Iterable, Optional, Reversible, Sequence, Tuple, Union
+
+from errors import UnexpectedTokenError
+from lex import Token, TokenTypes
+
+AllowedScalars = Union[bool, float, int, str]
+
+
+class VectorTypes(Enum):
+    """The different types of vectors that are allowed."""
+
+    LIST = auto()
+    TUPLE = auto()
+
+
+class ASTNode(ABC):
+    """
+    The base of all the nodes used in the AST.
+
+    Attributes
+    ----------
+    span: Tuple[int, int]
+        The position in the source text that this AST node came from.
+    type_: Optional[Type]
+        The type of the value that this AST node will eventually
+        evaluate to (default: `None`).
+    """
+
+    def __init__(self, span: Tuple[int, int]) -> None:
+        self.span: Tuple[int, int] = span
+        self.type_: Optional["Type"] = None
+
+    @abstractmethod
+    def visit(self, visitor):
+        """Run `visitor` on this node by selecting the correct node."""
+
+
+class Cond(ASTNode):
+    __slots__ = ("cons", "else_", "pred", "span", "type_")
+
+    def __init__(
+        self, span: Tuple[int, int], pred: ASTNode, cons: ASTNode, else_: ASTNode
+    ) -> None:
+        super().__init__(span)
+        self.pred: ASTNode = pred
+        self.cons: ASTNode = cons
+        self.else_: ASTNode = else_
+
+    def visit(self, visitor):
+        return visitor.visit_cond(self)
+
+
+class Define(ASTNode):
+    __slots__ = ("body", "span", "target", "type_", "value")
+
+    def __init__(
+        self,
+        span: Tuple[int, int],
+        target: "Name",
+        value: ASTNode,
+        body: Optional[ASTNode] = None,
+    ) -> None:
+        super().__init__(span)
+        self.target: Name = target
+        self.value: ASTNode = value
+        self.body: Optional[ASTNode] = body
+
+    def visit(self, visitor):
+        return visitor.visit_define(self)
+
+
+class FuncCall(ASTNode):
+    __slots__ = ("callee", "callee", "span", "type_")
+
+    def __init__(self, span: Tuple[int, int], caller: ASTNode, callee: ASTNode) -> None:
+        super().__init__(span)
+        self.caller: ASTNode = caller
+        self.callee: ASTNode = callee
+
+    def visit(self, visitor):
+        return visitor.visit_func_call(self)
+
+
+class Function(ASTNode):
+    __slots__ = ("body", "param", "span", "type_")
+
+    def __init__(self, span: Tuple[int, int], param: "Name", body: ASTNode) -> None:
+        super().__init__(span)
+        self.param: Name = param
+        self.body: ASTNode = body
+
+    @classmethod
+    def curry(cls, span: Tuple[int, int], params: Reversible["Name"], body: ASTNode):
+        """
+        Make a function which takes any number of arguments at once
+        into a series of nested ones that takes one arg at a time.
+
+        Warnings
+        --------
+        - This function assumes that the params list has been checked
+          to ensure it isn't empty.
+        """
+        for param in reversed(params):
+            body = cls(span, param, body)
+        return body
+
+    def visit(self, visitor):
+        return visitor.visit_function(self)
+
+
+class Name(ASTNode):
+    __slots__ = ("span", "type_", "value")
+
+    def __init__(
+        self, span: Tuple[int, int], value: str, type_: Optional["Type"] = None
+    ) -> None:
+        super().__init__(span)
+        self.value: str = value
+        self.type_: Optional[Type] = type_
+
+    @classmethod
+    def from_token(cls, token: Token, type_: Optional["Type"] = None):
+        """Create an instance of this node using a lexer token."""
+        if token.value is None:
+            raise UnexpectedTokenError(token, TokenTypes.name)
+        return cls(token.span, token.value, type_)
+
+    def visit(self, visitor):
+        return visitor.visit_name(self)
+
+    def __eq__(self, other):
+        return isinstance(other, Name) and self.value == other.value
+
+
+class Scalar(ASTNode):
+    __slots__ = ("span", "type_", "value")
+
+    def __init__(self, span: Tuple[int, int], value: AllowedScalars) -> None:
+        super().__init__(span)
+        self.value: AllowedScalars = value
+
+    def visit(self, visitor):
+        return visitor.visit_scalar(self)
+
+
+class Type(ASTNode, ABC):
+    """
+    This is the base class for the program's representation of types in
+    the type system.
+
+    Warnings
+    --------
+    - This class should not be used directly, instead use one of its
+      subclasses.
+    """
+
+    @abstractmethod
+    def is_concrete(self) -> bool:
+        """
+        Check whether the type is concrete or is made up of concrete
+        types.
+
+        Returns
+        -------
+        bool
+            If `True`, the type is concrete or made up of concrete
+            types.
+        """
+
+    def visit(self, visitor):
+        return visitor.visit_type(self)
+
+
+class FuncType(Type):
+    """
+    This is the type of a function for the type system.
+
+    Attributes
+    ----------
+    left: Type
+        The type of the single argument to the function.
+    right: Type
+        The type of what the function's return.
+    """
+
+    __slots__ = ("left", "span", "right", "type_")
+
+    def __init__(self, span: Tuple[int, int], left: Type, right: Type) -> None:
+        super().__init__(span)
+        self.left: Type = left
+        self.right: Type = right
+
+    def is_concrete(self) -> bool:
+        return self.left.is_concrete() and self.right.is_concrete()
+
+    def __eq__(self, other) -> bool:
+        return (
+            isinstance(other, FuncType)
+            and self.left == other.left
+            and self.right == other.right
+        )
+
+
+class GenericType(Type):
+
+    __slots__ = ("args", "base", "span", "type_")
+
+    def __init__(
+        self, span: Tuple[int, int], base: Name, args: Sequence[Type] = ()
+    ) -> None:
+        super().__init__(span)
+        self.base: Name = base
+        self.args: Sequence[Type] = args
+
+    def is_concrete(self) -> bool:
+        return all(map(lambda arg: arg.is_concrete(), self.args))
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, GenericType)
+            and self.base == other.base
+            and tuple(self.args) == tuple(other.args)
+        )
+
+
+class TypeVar(Type):
+
+    __slots__ = ("span", "type_", "value")
+    n_type_vars = 0
+
+    def __init__(self, span: Tuple[int, int], value: str) -> None:
+        super().__init__(span)
+        self.value: str = value
+
+    @classmethod
+    def unknown(cls, span: Tuple[int, int]):
+        """
+        Make a type var instance without explicitly providing a name
+        for it.
+
+        Attribute
+        ---------
+        span: Tuple[int, int]
+            The position of this instance in the source code.
+        """
+        cls.n_type_vars += 1
+        return cls(span, str(cls.n_type_vars))
+
+    @classmethod
+    def from_token(cls, token: Token):
+        """Create an instance of this node using a lexer token."""
+        if token.value is None:
+            raise UnexpectedTokenError(token, TokenTypes.name)
+        return cls(token.span, token.value)
+
+    def is_concrete(self) -> bool:
+        return False
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, TypeVar) and self.value == other.value
+
+
+class Vector(ASTNode):
+    __slots__ = ("elements", "span", "type_", "vec_type")
+
+    def __init__(
+        self, span: Tuple[int, int], vec_type: VectorTypes, elements: Iterable[ASTNode]
+    ) -> None:
+        super().__init__(span)
+        self.vec_type: VectorTypes = vec_type
+        self.elements: Iterable[ASTNode] = elements
+
+    def visit(self, visitor):
+        return visitor.visit_vector(self)
