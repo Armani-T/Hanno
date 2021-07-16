507908523c1beff9db2d22d1e0cf022647b77cd9
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-07-16 23:55:12 +0300

    Refactor `TypedASTPrinter`

--- hasdrubal/pprint_.py
+++ hasdrubal/pprint_.py
@@ -65,7 +65,7 @@ class ASTPrinter(NodeVisitor[str]):
         return bracket(", ".join((elem.visit(self) for elem in node.elements)))
 
 
-class TypedASTPrinter(NodeVisitor[str]):
+class TypedASTPrinter(ASTPrinter):
     """
     This visitor produces a string version of the entire AST with full
     type annotations.
@@ -75,68 +75,54 @@ class TypedASTPrinter(NodeVisitor[str]):
     This visitor assumes that the `type_` annotation is never `None`.
     """
 
-    def __init__(self) -> None:
-        self.indent_level: int = -1
-
-    def visit_block(self, node: ast.Block) -> str:
-        body = (f"# type: {node.type_.visit(self)}", node.first, *node.rest)
-        self.indent_level += 1
-        preface = f"\n{'  ' * self.indent_level}"
-        result = preface + preface.join((expr.visit(self) for expr in body))
-        self.indent_level -= 1
+    def visit_block(self, node):
+        result = node.first.visit(self)
+        if node.rest:
+            self.indent_level += 1
+            preface = f"\n{'  ' * self.indent_level}"
+            result = (
+                f"{preface}{result}{preface}"
+                f"{preface.join((expr.visit(self) for expr in node.rest))}"
+                f"{preface}# type: {node.type_.visit(self)}"
+            )
+            self.indent_level -= 1
         return result
 
-    def visit_cond(self, node: ast.Cond) -> str:
-        pred = node.pred.visit(self)
-        cons = node.cons.visit(self)
-        else_ = node.else_.visit(self)
+    def visit_cond(self, node):
         type_ = node.type_.visit(self)
-        return f"(if {pred} then {cons} else {else_}) :: {type_}"
+        return f"({super().visit_cond(node)}) :: {type_}"
 
-    def visit_define(self, node: ast.Define) -> str:
+    def visit_define(self, node):
         target = node.target.visit(self)
         value = node.value.visit(self)
-        body = (
-            ""
-            if node.body is None
-            else f" in {node.body.visit(self)} :: {node.type_.visit(self)}"
-        )
-        return f"let {target} = {value}{body}"
+        if node.body is not None:
+            return f"({super().visit_define(node)}) :: {node.type_.visit(self)}"
+        return f"let {target} :: {node.type_.visit(self)} = {value}"
 
-    def visit_func_call(self, node: ast.FuncCall) -> str:
+    def visit_func_call(self, node):
         type_ = node.type_.visit(self)
-        return f"{node.caller.visit(self)}( {node.callee.visit(self)} ) :: {type_}"
+        return f"({super().visit_func_call(node)}) :: {type_}"
 
-    def visit_function(self, node: ast.Function) -> str:
+    def visit_function(self, node):
         type_ = node.type_.visit(self)
         return f"(\\{node.param.visit(self)} -> {node.body.visit(self)}) :: {type_}"
 
-    def visit_name(self, node: ast.Name) -> str:
-        return f"{node.value} :: {node.type_.visit(self)}"
-
-    def visit_scalar(self, node: ast.Scalar) -> str:
+    def visit_scalar(self, node):
         return node.value_string
 
-    def visit_type(self, node: ast.Type) -> str:
+    def visit_type(self, node):
         if isinstance(node, TypeVar):
             return f"@{node.value}"
         if isinstance(node, FuncType):
             return f"{node.left.visit(self)} -> {node.right.visit(self)}"
         if isinstance(node, GenericType):
-            result = node.base.visit(self)
-            return (
-                f"({result}[{' '.join(map(lambda n: n.visit(self), node.args))}]"
-                if node.args
-                else result
-            )
+            result = node.base.value
+            if node.args:
+                result += f"[{' '.join((arg.visit(self) for arg in node.args))}]"
+            return result
         raise TypeError(
             f"{node} is an invalid subtype of nodes.Type, it is {type(node)}"
         )
 
-    def visit_vector(self, node: ast.Vector) -> str:
-        bracket = {
-            ast.VectorTypes.LIST: lambda string: f"[{string}]",
-            ast.VectorTypes.TUPLE: lambda string: f"({string})",
-        }[node.vec_type]
-        main = bracket(", ".join((elem.visit(self) for elem in node.elements)))
-        return f"{main} :: {node.type_.visit(self)}"
+    def visit_vector(self, node):
+        return f"{super().visit_vector(node)} :: {node.type_.visit(self)}"
