96c7bd4ed3cb5bd66b12c7b134fd94afc1d4ddc4
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-07-22 09:25:44 +0300

    Enable the logger to clear the log file every time it's opened

--- hasdrubal/log.py
+++ hasdrubal/log.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 _log_file = Path(__file__).parent.parent.joinpath("hasdrubal.log").resolve()
 # NOTE: Be careful with this value, it depends on the path to this file
-_log_file.touch(exist_ok=True)
+_log_file.touch()
 
 LOGGER_LEVEL = INFO
 
+_handler = FileHandler(_log_file, delay=True, mode="w")
 _formatter = Formatter(fmt="[%(levelname)s] %(message)s")
-_handler = FileHandler(_log_file, delay=True)
 _handler.setFormatter(_formatter)
 
 logger = getLogger()
