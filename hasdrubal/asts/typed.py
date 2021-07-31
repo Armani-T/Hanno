08a0df4af977ad4a9e1e5e5fa82ec5d41c6ced59
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-07-31 14:50:08 +0300

    Fix a bug caused by mass renaming a defined value.

--- hasdrubal/asts/typed.py
+++ hasdrubal/asts/typed.py
@@ -18,7 +18,7 @@ class TypedASTNode(base.ASTNode, ABC):
     """
 
     def __init__(self, span: base.Span, type_: Type) -> None:
-        super().__init__(base.Span)
+        super().__init__(span)
         self.type_: "Type" = type_
 
 
@@ -31,8 +31,8 @@ class Block(base.Block, TypedASTNode):
         type_: Type,
         body: Sequence[TypedASTNode],
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Block.__init__(self, base.Span, body)
+        TypedASTNode.__init__(self, span, type_)
+        base.Block.__init__(self, span, body)
 
 
 class Cond(base.Cond, TypedASTNode):
@@ -46,8 +46,8 @@ class Cond(base.Cond, TypedASTNode):
         cons: TypedASTNode,
         else_: TypedASTNode,
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Cond.__init__(self, base.Span, pred, cons, else_)
+        TypedASTNode.__init__(self, span, type_)
+        base.Cond.__init__(self, span, pred, cons, else_)
 
 
 class Define(base.Define, TypedASTNode):
@@ -61,8 +61,8 @@ class Define(base.Define, TypedASTNode):
         value: TypedASTNode,
         body: Optional[TypedASTNode] = None,
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Define.__init__(self, base.Span, target, value, body)
+        TypedASTNode.__init__(self, span, type_)
+        base.Define.__init__(self, span, target, value, body)
 
 
 class FuncCall(base.FuncCall, TypedASTNode):
@@ -75,8 +75,8 @@ class FuncCall(base.FuncCall, TypedASTNode):
         caller: TypedASTNode,
         callee: TypedASTNode,
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.FuncCall.__init__(self, base.Span, caller, callee)
+        TypedASTNode.__init__(self, span, type_)
+        base.FuncCall.__init__(self, span, caller, callee)
 
 
 class Function(base.Function, TypedASTNode):
@@ -89,14 +89,14 @@ class Function(base.Function, TypedASTNode):
         param: "Name",
         body: TypedASTNode,
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Function.__init__(self, base.Span, param, body)
+        TypedASTNode.__init__(self, span, type_)
+        base.Function.__init__(self, span, param, body)
 
     @classmethod
     def curry(cls, span: base.Span, params: Reversible["Name"], body: TypedASTNode):
         for param in reversed(params):
             body = cls(
-                base.Span,
+                span,
                 FuncType(base.Span, param.type_, body.type_),
                 param,
                 body,
@@ -108,8 +108,8 @@ class Name(base.Name, TypedASTNode):
     __slots__ = ("span", "type_", "value")
 
     def __init__(self, span: base.Span, type_: Type, value: Optional[str]) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Name.__init__(self, base.Span, value)
+        TypedASTNode.__init__(self, span, type_)
+        base.Name.__init__(self, span, value)
 
 
 class Scalar(base.Scalar, TypedASTNode):
@@ -122,8 +122,8 @@ class Scalar(base.Scalar, TypedASTNode):
         scalar_type: base.ScalarTypes,
         value_string: Optional[str],
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Scalar.__init__(self, base.Span, scalar_type, value_string)
+        TypedASTNode.__init__(self, span, type_)
+        base.Scalar.__init__(self, span, scalar_type, value_string)
 
 
 class Vector(base.Vector, TypedASTNode):
@@ -136,13 +136,13 @@ class Vector(base.Vector, TypedASTNode):
         vec_type: base.VectorTypes,
         elements: Iterable[TypedASTNode],
     ) -> None:
-        TypedASTNode.__init__(self, base.Span, type_)
-        base.Vector.__init__(self, base.Span, vec_type, elements)
+        TypedASTNode.__init__(self, span, type_)
+        base.Vector.__init__(self, span, vec_type, elements)
 
     @classmethod
     def unit(cls, span: base.Span):
         return cls(
-            base.Span,
+            span,
             GenericType(base.Span, base.Name(base.Span, "Unit")),
             base.VectorTypes.TUPLE,
             (),
