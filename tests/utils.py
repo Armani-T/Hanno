ec8eb2eaabb021e0d4b44fd5ab939a05d10a7e9d
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-08-03 12:27:25 +0300

    Update the tests with the new syntax

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,7 @@ from context import base, pprint_, types
 base.ASTNode.__repr__ = lambda node: node.visit(pprint_.ASTPrinter())
 types.Type.__repr__ = lambda node: pprint_.show_type(node, True)
 
-SAMPLE_SOURCE = "let l = [1, 2, 3] <> [4, 5, 6] in head(l)"
+SAMPLE_SOURCE = "let l = [1, 2, 3] <> [4, 5, 6]\nhead(l)\n"
 SAMPLE_SOURCE_PATH = __file__
 
 
