6d92c646d06079980b31aecb5182c06b03dd3918
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-08-21 19:48:44 +0300

    Simplify the instruction set by merging all the `LOAD_*`s

--- hasdrubal/codegen.py
+++ hasdrubal/codegen.py
@@ -7,19 +7,16 @@ Instruction = namedtuple("Instruction", ("opcode", "operands"))
 @unique
 class OpCodes(Enum):
     EXIT = 0
-    LOAD_NAME = 1
-    LOAD_INT = 2
-    LOAD_STR = 3
-    LOAD_FLOAT = 4
-    LOAD_BOOL = 5
-    LOAD_FUNC = 6
 
-    BUILD_TUPLE = 7
-    BUILD_LIST = 8
+    LOAD_VAL = 1
+    LOAD_VAR = 2
 
-    CALL = 11
+    BUILD_TUPLE = 3
+    BUILD_LIST = 4
 
-    STORE = 12
+    CALL = 5
 
-    BRANCH = 13
-    BRANCH_FALSE = 14
+    STORE_VAR = 6
+
+    BRANCH = 7
+    BRANCH_FALSE = 8
