4b7c899d36e7bc8f2a25babb90966ad369a15c9e
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-08-21 19:16:33 +0300

    Add the other bytecode instructions
    These are all the instructions needed in order to run any code written
    in Hasdrubal at the moment.
    
    I also renamed the values inside `Instruction` to something more 
    contextually accurate.

--- hasdrubal/codegen.py
+++ hasdrubal/codegen.py
@@ -1,11 +1,11 @@
 from collections import namedtuple
 from enum import Enum, unique
 
-Instruction = namedtuple("Instruction", ("code", "args"))
+Instruction = namedtuple("Instruction", ("opcode", "operands"))
 
 
 @unique
-class ByteCodes(Enum):
+class OpCodes(Enum):
     EXIT = 0
     LOAD_NAME = 1
     LOAD_INT = 2
@@ -13,3 +13,13 @@ class ByteCodes(Enum):
     LOAD_FLOAT = 4
     LOAD_BOOL = 5
     LOAD_FUNC = 6
+
+    BUILD_TUPLE = 7
+    BUILD_LIST = 8
+
+    CALL = 11
+
+    STORE = 12
+
+    BRANCH = 13
+    BRANCH_FALSE = 14
