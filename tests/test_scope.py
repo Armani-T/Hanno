d4342f01fdbd3fd29fbf03ab90f4e904e0f84410
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-09-20 12:13:23 +0300

    Add tests for `Scope.depth`

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -3,8 +3,6 @@ from pytest import raises
 
 from context import base, errors, scope
 
-SAMPLE_SCOPE = scope.Scope(scope.DEFAULT_OPERATOR_TYPES)
-
 
 def test_scope_down():
     parent = scope.Scope(None)
@@ -29,3 +27,47 @@ def test_scope_up_success():
     result = sample.up()
     assert result is not sample
     assert result is sample._parent
+
+
+def test_scope_depth_with_undefined_name():
+    name = base.Name((0, 1), "<+>")
+    assert scope.DEFAULT_OPERATOR_TYPES.depth(name) == -1
+
+
+def test_scope_depth_with_no_nesting():
+    name = base.Name((0, 1), "+")
+    assert scope.DEFAULT_OPERATOR_TYPES.depth(name) == 0
+
+
+def test_scope_depth_with_nested_2():
+    parent = scope.Scope(None)
+    name = base.Name((0, 6), "my_var")
+    parent[name] = base.Scalar((10, 12), 42)
+    child = parent.down()
+    assert child.depth(name) == 1
+
+
+def test_scope_depth_with_nested_5():
+    parent = scope.Scope(None)
+    name = base.Name((0, 6), "my_var")
+    parent[name] = base.Scalar((10, 12), 42)
+    child = parent
+    for _ in range(5):
+        child = child.down()
+
+    assert child.depth(name) == 5
+
+
+def test_scope_depth_with_shadowing():
+    parent = scope.Scope(None)
+    upper_name = base.Name((0, 6), "my_var")
+    parent[upper_name] = base.Scalar((10, 12), 42)
+    child = parent
+    for _ in range(4):
+        child = child.down()
+
+    lower_name = base.Name((238, 244), "my_var")
+    child[lower_name] = base.Scalar((10, 12), 67)
+    child = child.down()
+    assert child.depth(upper_name) == 1
+    assert child.depth(lower_name) == 1
