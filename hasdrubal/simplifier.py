d860892ac6eae97aa5a9b79bd12a4e202aed9641
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-10-17 08:19:03 +0300

    Update the simplifier to use the new `asts.lowered` API

--- hasdrubal/simplifier.py
+++ hasdrubal/simplifier.py
@@ -1,4 +1,4 @@
-from typing import Container
+from typing import Union
 
 from asts import base, lowered, visitor
 from errors import FatalInternalError
@@ -25,16 +25,18 @@ def simplify(node: base.ASTNode) -> lowered.LoweredASTNode:
     return simplifier.run(node)
 
 
-def _fold_calls(call: base.FuncCall) -> lowered.FuncCall:
+def _fold_calls(
+    call: base.FuncCall,
+) -> tuple[lowered.LoweredASTNode, list[lowered.Name]]:
     original_span = call.span
     args = []
     while isinstance(call, base.FuncCall):
         args.append(call.callee)
         call = call.caller
-    return lowered.FuncCall(original_span, call, args)
+    return call, args
 
 
-class ASTSimplifier(visitor.BaseASTVisitor[base.ASTNode]):
+class ASTSimplifier(visitor.BaseASTVisitor[lowered.LoweredASTNode]):
     """
     Convert undefined `TypeName`s into `TypeVar`s using `defined_types`
     as a kind of symbol table to check whether a name should remain
@@ -46,32 +48,36 @@ class ASTSimplifier(visitor.BaseASTVisitor[base.ASTNode]):
         The identifiers that are known to actually be type names.
     """
 
-    def visit_block(self, node: base.Block) -> base.Block:
-        return base.Block(node.span, [expr.visit(self) for expr in node.body()])
+    def visit_block(self, node: base.Block) -> lowered.Block:
+        return lowered.Block(node.span, [expr.visit(self) for expr in node.body()])
 
-    def visit_cond(self, node: base.Cond) -> base.Cond:
-        return base.Cond(
+    def visit_cond(self, node: base.Cond) -> lowered.Cond:
+        return lowered.Cond(
             node.span,
             node.pred.visit(self),
             node.cons.visit(self),
             node.else_.visit(self),
         )
 
-    def visit_define(self, node: base.Define) -> base.Define:
-        return base.Define(node.span, node.target.visit(self), node.value.visit(self))
+    def visit_define(self, node: base.Define) -> lowered.Define:
+        return lowered.Define(
+            node.span, node.target.visit(self), node.value.visit(self)
+        )
 
-    def visit_func_call(self, node: base.FuncCall):
-        result = _fold_calls(node)
-        result.func = result.func.visit(self)
-        result.args = [arg.visit(self) for arg in result.args]
+    def visit_func_call(
+        self, node: base.FuncCall
+    ) -> Union[lowered.FuncCall, lowered.NativeOperation]:
+        func, args = _fold_calls(node)
+        func = func.visit(self)
+        args = [arg.visit(self) for arg in args]
 
         try:
-            operator = lowered.OperationTypes(result.func.value)
-            left = result.args[0]
-            right = result.args[0] if len(result.args) > 1 else None
-            return NativeOperation(node.span, operator, left, right)
+            operator = lowered.OperationTypes(func.value)
+            left = args[0]
+            right = args[1] if len(args) > 1 else None
+            return lowered.NativeOperation(node.span, operator, left, right)
         except (ValueError, AttributeError):
-            return result
+            return lowered.FuncCall(node.span, func, args)
 
     def visit_function(self, node: base.Function) -> base.Function:
         original_span = node.span
@@ -79,22 +85,20 @@ class ASTSimplifier(visitor.BaseASTVisitor[base.ASTNode]):
         while isinstance(node, base.Function):
             params.append(node.params)
             node = node.body
-        return lowered.Function(original_span, params, body)
+        return lowered.Function(original_span, params, node)
 
     def visit_name(self, node: base.Name) -> base.Name:
-        return base.Name(node.span, node.value)
+        return lowered.Name(node.span, node.value)
 
-    def visit_scalar(self, node: base.Scalar) -> base.Scalar:
+    def visit_scalar(self, node: base.Scalar) -> lowered.Scalar:
         return node
 
     def visit_type(self, node):
-        logger.fatal(
-            "Tried to convert this `Type` node (%r) in the AST to bytecode.", node
-        )
+        logger.fatal("Tried to simplify this `Type` node in the AST: %r", node)
         raise FatalInternalError()
 
-    def visit_vector(self, node: base.Vector) -> base.Vector:
-        return base.Vector(
+    def visit_vector(self, node: base.Vector) -> lowered.Vector:
+        return lowered.Vector(
             node.span,
             node.vec_type,
             [element.visit(self) for element in node.elements],
