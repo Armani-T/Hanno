53b24dd50ba577bdb5fbfd8fb0f8eb4ea9c51cc4
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-10-18 16:01:52 +0300

    Refactor the code to take into the account the config data at more points

--- hasdrubal/run.py
+++ hasdrubal/run.py
@@ -11,49 +11,59 @@ from pprint_ import ASTPrinter, TypedASTPrinter
 from type_inferer import infer_types
 from type_var_resolver import resolve_type_vars
 
+identity = lambda x: x
+pipe = partial(reduce, lambda arg, func: func(arg))
 to_string: Callable[[Union[bytes, str]], str] = lambda text: (
     text if isinstance(text, str) else to_utf8(text, "utf8")
 )
 
-pipe = partial(reduce, lambda arg, func: func(arg))
-
-functions = {
+function_generator = lambda config: {
     "lexing": {
         "before": (to_string, normalise_newlines),
         "main": lex,
         "after": (infer_eols,),
-        "stop": lambda config: (config.show_tokens, show_tokens),
+        "should_stop": config.show_tokens,
+        "on_stop": show_tokens,
     },
     "parsing": {
         "before": (TokenStream,),
         "main": parse,
         "after": (),
-        "stop": lambda config: (config.show_ast, ASTPrinter().run),
+        "should_stop": config.show_ast,
+        "on_stop": ASTPrinter().run,
     },
     "type_checking": {
-        "before": (resolve_type_vars, topological_sort),
+        "before": (
+            resolve_type_vars,
+            topological_sort if config.sort_defs else identity,
+        ),
         "main": infer_types,
         "after": (),
-        "stop": lambda config: (config.show_types, TypedASTPrinter().run),
+        "should_stop": config.show_types,
+        "on_stop": TypedASTPrinter().run,
     },
     "codegen": {
         "before": (),
         "main": to_bytecode,
         "after": (),
-        "stop": lambda _, __: (False, lambda _: None),
+        "should_stop": False,
+        "stop_callback": lambda _: None,
     },
 }
 
 
-def run_phase(phase: str, initial: Any, config: ConfigData) -> Any:
-    tasks = functions[phase]
-    prepared_value = pipe(tasks["before"], initial)
-    main_func = tasks["main"]
-    main_value = main_func(prepared_value)
-    processed_value = pipe(tasks["after"], main_value)
-    stop_func = tasks["stop"]
-    stop, callback = stop_func(config)
-    return stop, callback, processed_value
+def build_phase_runner(config: ConfigData):
+    functions = function_generator(config)
+
+    def inner(phase: str, initial: Any) -> Any:
+        tasks = functions[phase]
+        prepared_value = pipe(tasks["before"], initial)
+        main_func = tasks["main"]
+        main_value = main_func(prepared_value)
+        processed_value = pipe(tasks["after"], main_value)
+        return tasks["should_stop"], tasks["on_stop"], processed_value
+
+    return inner
 
 
 def run_code(source: Union[bytes, str], config: ConfigData) -> str:
@@ -76,12 +86,12 @@ def run_code(source: Union[bytes, str], config: ConfigData) -> str:
     """
     report, _ = config.writers
     try:
-        result = source
+        run_phase = build_phase_runner(config)
         phases = ("lexing", "parsing", "type_checking", "codegen")
         for phase in phases:
-            stop, callback, result = run_phase(phase, result, config)
+            stop, callback, source = run_phase(phase, source)
             if stop:
-                return callback(result)
+                return callback(source)
 
         return ""
     except KeyboardInterrupt:
