f13012b35350d7497d892359c687855d64b54e5e
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-11-09 12:28:44 +0300

    Add tests for the general case in the constant folder
    These tests are currently failing because there seems to be a bug in
    the constant folding code.

---
+++ tests/test_constant_folder.py
@@ -0,0 +1,69 @@
+from pytest import mark
+
+from context import constant_folder, lowered
+import utils
+
+span = (0, 0)
+
+
+@mark.constant_folding
+@mark.optimisation
+@mark.parametrize(
+    "source,expected",
+    (
+        (
+            lowered.NativeOperation(
+                span,
+                lowered.OperationTypes.SUB,
+                lowered.Scalar(span, 7),
+                lowered.Scalar(span, 3),
+            ),
+            lowered.Scalar(span, 7 - 3),
+        ),
+        (
+            lowered.NativeOperation(
+                span,
+                lowered.OperationTypes.ADD,
+                lowered.NativeOperation(
+                    span,
+                    lowered.OperationTypes.MUL,
+                    lowered.Scalar(span, 14),
+                    lowered.Scalar(span, 2),
+                ),
+                lowered.Scalar(span, 3),
+            ),
+            lowered.Scalar(span, 14 * 2 + 3),
+        ),
+        (
+            lowered.Block(
+                span,
+                (
+                    lowered.Define(
+                        span, lowered.Name(span, "x"), lowered.Scalar(span, 12)
+                    ),
+                    lowered.NativeOperation(
+                        span,
+                        lowered.OperationTypes.DIV,
+                        lowered.Scalar(span, 1),
+                        lowered.NativeOperation(
+                            span,
+                            lowered.OperationTypes.EXP,
+                            lowered.Name(span, "x"),
+                            lowered.Scalar(span, 2),
+                        ),
+                    ),
+                ),
+            ),
+            lowered.Block(
+                span,
+                [
+                    lowered.Vector.unit(span),
+                    lowered.Scalar(span, 1 // (12 ** 2)),
+                ],
+            ),
+        ),
+    ),
+)
+def test_fold_constants(source, expected):
+    actual = constant_folder.fold_constants(source)
+    assert expected == actual
