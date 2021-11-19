45188d67b1301bf8913ebab64aee2df68e7fce10
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-11-19 16:44:45 +0300

    Add `inline_expander` to the testing context
    So that I can begin writing tests for the module

--- tests/context.py
+++ tests/context.py
@@ -11,14 +11,15 @@ import ast_sorter as sorter
 import codegen
 import constant_folder
 import errors
+import inline_expander
 import lex
 import parse_ as parse
-import pprint_
+import pprint_ as pprint
 import scope
 import type_inferer
 import type_var_resolver
 
-base.ASTNode.__repr__ = lambda node: node.visit(pprint_.ASTPrinter())
-types.Type.__repr__ = lambda node: pprint_.show_type(node, True)
-typed.TypedASTNode.__repr__ = lambda node: node.visit(pprint_.TypedASTPrinter())
-lowered.LoweredASTNode.__repr__ = lambda node: node.visit(pprint_.LoweredASTPrinter())
+base.ASTNode.__repr__ = lambda node: node.visit(pprint.ASTPrinter())
+types.Type.__repr__ = lambda node: pprint.show_type(node, True)
+typed.TypedASTNode.__repr__ = lambda node: node.visit(pprint.TypedASTPrinter())
+lowered.LoweredASTNode.__repr__ = lambda node: node.visit(pprint.LoweredASTPrinter())
