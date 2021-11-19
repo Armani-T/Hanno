55d7d182a375ba9c999dc6ef9df86a43c5b45df2
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-11-19 17:29:07 +0300

    Add tests for the `_Scorer`

--- tests/test_inline_expander.py
+++ tests/test_inline_expander.py
@@ -23,3 +23,90 @@ identity_func = lowered.Function(
 def test_generate_scores(funcs, defined, threshold, expected):
     actual = inline_expander.generate_scores(funcs, defined, threshold)
     assert expected == actual
+
+
+@mark.inline_expansion
+@mark.optimisation
+@mark.parametrize(
+    "tree,expected",
+    (
+        (lowered.Scalar(span, 1.25), 0),
+        (lowered.Name(span, "map"), 0),
+        (identity_func, 7),
+        (
+            lowered.Block(
+                span,
+                [
+                    lowered.Define(
+                        span,
+                        lowered.Name(span, "x"),
+                        lowered.Scalar(span, 24),
+                    ),
+                    lowered.Define(
+                        span,
+                        lowered.Name(span, "y"),
+                        lowered.NativeOperation(
+                            span,
+                            lowered.OperationTypes.EXP,
+                            lowered.Name(span, "x"),
+                            lowered.Scalar(span, 2),
+                        ),
+                    ),
+                    lowered.NativeOperation(
+                        span,
+                        lowered.OperationTypes.DIV,
+                        lowered.Name(span, "x"),
+                        lowered.Name(span, "y"),
+                    ),
+                ],
+            ),
+            15,
+        ),
+        (
+            lowered.Define(
+                span,
+                lowered.Name(span, "collatz"),
+                lowered.Function(
+                    span,
+                    [lowered.Name(span, "n")],
+                    lowered.Cond(
+                        span,
+                        lowered.NativeOperation(
+                            span,
+                            lowered.OperationTypes.EQUAL,
+                            lowered.NativeOperation(
+                                span,
+                                lowered.OperationTypes.MOD,
+                                lowered.Name(span, "n"),
+                                lowered.Scalar(span, 2),
+                            ),
+                            lowered.Scalar(span, 2),
+                        ),
+                        lowered.NativeOperation(
+                            span,
+                            lowered.OperationTypes.DIV,
+                            lowered.Name(span, "n"),
+                            lowered.Scalar(span, 2),
+                        ),
+                        lowered.NativeOperation(
+                            span,
+                            lowered.OperationTypes.ADD,
+                            lowered.NativeOperation(
+                                span,
+                                lowered.OperationTypes.MUL,
+                                lowered.Scalar(span, 3),
+                                lowered.Name(span, "n"),
+                            ),
+                            lowered.Scalar(span, 1),
+                        ),
+                    )
+                )
+            ),
+            22,
+        ),
+    ),
+)
+def test_scorer(tree, expected):
+    scorer = inline_expander._Scorer()
+    actual = scorer.run(tree)
+    assert expected == actual
