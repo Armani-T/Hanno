8dc222b9d92a8adb09dd0b67a19fcccce6f0c7c8
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-11-19 14:34:38 +0300

    Change the function inliner to work on the lowered AST instead

--- hasdrubal/function_inliner.py
+++ hasdrubal/function_inliner.py
@@ -1,17 +1,19 @@
 from typing import Container, List, Mapping, Sequence, Set, Tuple
 
-from asts.types_ import Type
-from asts import base, visitor
+from asts.base import VectorTypes
+from asts import lowered, visitor
 from scope import Scope
 
 
-def inline_functions(tree: base.ASTNode, threshold: int = 25) -> base.ASTNode:
+def inline_functions(
+    tree: lowered.LoweredASTNode, threshold: int = 25
+) -> lowered.LoweredASTNode:
     """
     Inline unnecessary or trivial functions to make the program run faster.
 
     Parameters
     ----------
-    tree: base.ASTNode
+    tree: lowered.LoweredASTNode
         The tree without any inlined functions.
     threshold: int
         The number that determines how aggressive the inlining should
@@ -19,7 +21,7 @@ def inline_functions(tree: base.ASTNode, threshold: int = 25) -> base.ASTNode:
 
     Returns
     -------
-    base.ASTNode
+    lowered.LoweredASTNode
         The tree with as many functions inlines as is reasonable.
     """
     finder = _Finder()
@@ -29,7 +31,7 @@ def inline_functions(tree: base.ASTNode, threshold: int = 25) -> base.ASTNode:
     return inliner.run(tree)
 
 
-class _Scorer(visitor.BaseASTVisitor[int]):
+class _Scorer(visitor.LoweredASTVisitor[int]):
     """
     A visitor that gives a numeric weight to a piece of the AST.
 
@@ -37,188 +39,211 @@ class _Scorer(visitor.BaseASTVisitor[int]):
     conditionals compared to simple names.
     """
 
-    def visit_block(self, node: base.Block) -> int:
+    def visit_block(self, node: lowered.Block) -> int:
         return 2 + sum(expr.visit(self) for expr in node.body)
 
-    def visit_cond(self, node: base.Cond) -> int:
+    def visit_cond(self, node: lowered.Cond) -> int:
         return (
             3 + node.pred.visit(self) + node.cons.visit(self) + node.else_.visit(self)
         )
 
-    def visit_define(self, node: base.Define) -> int:
+    def visit_define(self, node: lowered.Define) -> int:
         return 2 + node.value.visit(self)
 
-    def visit_func_call(self, node: base.FuncCall) -> int:
-        return node.caller.visit(self) + node.callee.visit(self)
+    def visit_func_call(self, node: lowered.FuncCall) -> int:
+        return node.func.visit(self) + sum(map(self.run, node.args))
 
-    def visit_function(self, node: base.Function) -> int:
+    def visit_function(self, node: lowered.Function) -> int:
         return 5 + node.body.visit(self)
 
-    def visit_name(self, node: base.Name) -> int:
+    def visit_name(self, node: lowered.Name) -> int:
         return 1
 
-    def visit_scalar(self, node: base.Scalar) -> int:
+    def visit_native_operation(self, node: lowered.NativeOperation) -> int:
+        return (
+            1
+            + node.left.visit(self)
+            + (0 if node.right is None else node.right.visit(self))
+        )
+
+    def visit_scalar(self, node: lowered.Scalar) -> int:
         return 1
 
-    def visit_type(self, node: Type) -> int:
-        return 0
-
-    def visit_vector(self, node: base.Vector) -> int:
-        type_weight = 2 if node.vec_type == base.VectorTypes.LIST else 1
+    def visit_vector(self, node: lowered.Vector) -> int:
+        type_weight = 2 if node.vec_type == VectorTypes.LIST else 1
         return type_weight + sum(elem.visit(self) for elem in node.elements)
 
 
-class _Finder(visitor.BaseASTVisitor[None]):
+class _Finder(visitor.LoweredASTVisitor[None]):
     def __init__(self) -> None:
-        self.funcs: List[base.Function] = []
-        self.defined_funcs: Set[base.Function] = set()
+        self.funcs: List[lowered.Function] = []
+        self.defined_funcs: Set[lowered.Function] = set()
 
-    def visit_block(self, node: base.Block) -> None:
+    def visit_block(self, node: lowered.Block) -> None:
         for expr in node.body:
             expr.visit(self)
 
-    def visit_cond(self, node: base.Cond) -> None:
+    def visit_cond(self, node: lowered.Cond) -> None:
         node.pred.visit(self)
         node.cons.visit(self)
         node.else_.visit(self)
 
-    def visit_define(self, node: base.Define) -> None:
+    def visit_define(self, node: lowered.Define) -> None:
         node.value.visit(self)
-        if isinstance(node.value, base.Function):
+        if isinstance(node.value, lowered.Function):
             self.defined_funcs.add(node.value)
 
-    def visit_func_call(self, node: base.FuncCall) -> None:
-        node.caller.visit(self)
-        node.callee.visit(self)
+    def visit_func_call(self, node: lowered.FuncCall) -> None:
+        node.func.visit(self)
+        for arg in node.args:
+            arg.visit(self)
 
-    def visit_function(self, node: base.Function) -> None:
+    def visit_function(self, node: lowered.Function) -> None:
         node.body.visit(self)
         self.funcs.append(node)
 
-    def visit_name(self, node: base.Name) -> None:
+    def visit_name(self, node: lowered.Name) -> None:
         return
 
-    def visit_scalar(self, node: base.Scalar) -> None:
+    def visit_native_operation(self, node: lowered.NativeOperation) -> None:
+        node.left.visit(self)
+        if node.right is not None:
+            node.right.visit(self)
+
+    def visit_scalar(self, node: lowered.Scalar) -> None:
         return
 
-    def visit_type(self, node: Type) -> None:
-        return
-
-    def visit_vector(self, node: base.Vector) -> None:
+    def visit_vector(self, node: lowered.Vector) -> None:
         for elem in node.elements:
             elem.visit(self)
 
 
-class _Inliner(visitor.BaseASTVisitor[base.ASTNode]):
-    def __init__(self, scores: Mapping[base.Function, int], threshold: int) -> None:
-        self.current_scope: Scope[base.Function] = Scope(None)
-        self.scores: Mapping[base.Function, int] = {
+class _Inliner(visitor.LoweredASTVisitor[lowered.LoweredASTNode]):
+    def __init__(self, scores: Mapping[lowered.Function, int], threshold: int) -> None:
+        self.current_scope: Scope[lowered.Function] = Scope(None)
+        self.scores: Mapping[lowered.Function, int] = {
             func: score for func, score in scores.items() if score <= threshold
         }
 
-    def visit_block(self, node: base.Block) -> base.Block:
-        return base.Block(node.span, [expr.visit(self) for expr in node.body])
+    def visit_block(self, node: lowered.Block) -> lowered.Block:
+        return lowered.Block(node.span, [expr.visit(self) for expr in node.body])
 
-    def visit_cond(self, node: base.Cond) -> base.Cond:
-        return base.Cond(
+    def visit_cond(self, node: lowered.Cond) -> lowered.Cond:
+        return lowered.Cond(
             node.span,
             node.pred.visit(self),
             node.cons.visit(self),
             node.else_.visit(self),
         )
 
-    def visit_define(self, node: base.Define) -> base.Define:
+    def visit_define(self, node: lowered.Define) -> lowered.Define:
         value = node.value.visit(self)
-        if isinstance(value, base.Function):
+        if isinstance(value, lowered.Function):
             self.current_scope[node.target] = value
-        return base.Define(node.span, node.target, value)
+        return lowered.Define(node.span, node.target, value)
 
-    def visit_func_call(self, node: base.FuncCall) -> base.ASTNode:
-        caller, callee = node.caller.visit(self), node.callee.visit(self)
-        if isinstance(caller, base.Name) and caller in self.current_scope:
-            actual_function = self.current_scope[caller]
-            return inline(node.span, actual_function, callee)
-        if isinstance(caller, base.Function) and caller in self.scores:
-            return inline(node.span, caller, callee)
-        return base.FuncCall(node.span, caller, callee)
+    def visit_func_call(self, node: lowered.FuncCall) -> lowered.LoweredASTNode:
+        func = node.func.visit(self)
+        args = [arg.visit(self) for arg in node.args]
+        if isinstance(func, lowered.Name) and func in self.current_scope:
+            actual_function = self.current_scope[func]
+            return inline(node.span, actual_function, args)
+        if isinstance(func, lowered.Function) and func in self.scores:
+            return inline(node.span, func, args)
+        return lowered.FuncCall(node.span, func, args)
 
-    def visit_function(self, node: base.Function) -> base.Function:
-        return base.Function(node.span, node.param, node.body.visit(self))
+    def visit_function(self, node: lowered.Function) -> lowered.Function:
+        return lowered.Function(node.span, node.params, node.body.visit(self))
 
-    def visit_name(self, node: base.Name) -> base.Name:
+    def visit_name(self, node: lowered.Name) -> lowered.Name:
         return node
 
-    def visit_scalar(self, node: base.Scalar) -> base.Scalar:
+    def visit_native_operation(
+        self, node: lowered.NativeOperation
+    ) -> lowered.NativeOperation:
+        return lowered.NativeOperation(
+            node.span,
+            node.operation,
+            node.left.visit(self),
+            None if node.right is None else node.right.visit(self),
+        )
+
+    def visit_scalar(self, node: lowered.Scalar) -> lowered.Scalar:
         return node
 
-    def visit_type(self, node: Type) -> Type:
-        return node
-
-    def visit_vector(self, node: base.Vector) -> base.Vector:
-        return base.Vector(
+    def visit_vector(self, node: lowered.Vector) -> lowered.Vector:
+        return lowered.Vector(
             node.span, node.vec_type, [elem.visit(self) for elem in node.elements]
         )
 
 
-class _Replacer(visitor.BaseASTVisitor[base.ASTNode]):
-    def __init__(self, name: base.Name, value: base.ASTNode) -> None:
-        self.name: base.Name = name
-        self.value: base.ASTNode = value
+class _Replacer(visitor.LoweredASTVisitor[lowered.LoweredASTNode]):
+    def __init__(self, inlined: Scope[lowered.LoweredASTNode]) -> None:
+        self.inlined: Scope[lowered.LoweredASTNode] = inlined
 
-    def visit_block(self, node: base.Block) -> base.Block:
-        return base.Block(node.span, [expr.visit(self) for expr in node.body])
+    def visit_block(self, node: lowered.Block) -> lowered.Block:
+        return lowered.Block(node.span, [expr.visit(self) for expr in node.body])
 
-    def visit_cond(self, node: base.Cond) -> base.Cond:
-        return base.Cond(
+    def visit_cond(self, node: lowered.Cond) -> lowered.Cond:
+        return lowered.Cond(
             node.span,
             node.pred.visit(self),
             node.cons.visit(self),
             node.else_.visit(self),
         )
 
-    def visit_define(self, node: base.Define) -> base.Define:
-        return base.Define(node.span, node.target, node.value.visit(self))
+    def visit_define(self, node: lowered.Define) -> lowered.Define:
+        return lowered.Define(node.span, node.target, node.value.visit(self))
 
-    def visit_func_call(self, node: base.FuncCall) -> base.ASTNode:
-        return base.FuncCall(
-            node.span, node.caller.visit(self), node.callee.visit(self)
+    def visit_func_call(self, node: lowered.FuncCall) -> lowered.LoweredASTNode:
+        return lowered.FuncCall(
+            node.span,
+            node.func.visit(self),
+            [arg.visit(self) for arg in node.args],
         )
 
-    def visit_function(self, node: base.Function) -> base.Function:
-        return base.Function(node.span, node.param, node.body.visit(self))
+    def visit_function(self, node: lowered.Function) -> lowered.Function:
+        return lowered.Function(node.span, node.params, node.body.visit(self))
 
-    def visit_name(self, node: base.Name) -> base.ASTNode:
-        return self.value if node == self.name else node
+    def visit_name(self, node: lowered.Name) -> lowered.LoweredASTNode:
+        return self.inlined[node] if node in self.inlined else node
 
-    def visit_scalar(self, node: base.Scalar) -> base.Scalar:
+    def visit_native_operation(
+        self, node: lowered.NativeOperation
+    ) -> lowered.NativeOperation:
+        return lowered.NativeOperation(
+            node.span,
+            node.operation,
+            node.left.visit(self),
+            None if node.right is None else node.right.visit(self),
+        )
+
+    def visit_scalar(self, node: lowered.Scalar) -> lowered.Scalar:
         return node
 
-    def visit_type(self, node: Type) -> Type:
-        return node
-
-    def visit_vector(self, node: base.Vector) -> base.Vector:
-        return base.Vector(
+    def visit_vector(self, node: lowered.Vector) -> lowered.Vector:
+        return lowered.Vector(
             node.span, node.vec_type, [elem.visit(self) for elem in node.elements]
         )
 
 
 def generate_scores(
-    funcs: Sequence[base.Function], defined_funcs: Container[base.Function]
-) -> Mapping[base.Function, int]:
+    funcs: Sequence[lowered.Function], defined_funcs: Container[lowered.Function]
+) -> Mapping[lowered.Function, int]:
     """
     Generate the total inlining score for every function found in the
     AST.
 
     Parameters
     ----------
-    funcs: Sequence[base.Function]
+    funcs: Sequence[lowered.Function]
         All the `Function` nodes found in the AST.
-    defined_funcs: Container[base.Function]
+    defined_funcs: Container[lowered.Function]
         A set of functions that are directly tied to a `Define` node.
 
     Returns
     -------
-    Mapping[base.Function, int]
+    Mapping[lowered.Function, int]
         A mapping between each of those function nodes and their
         overall scores.
     """
@@ -229,10 +254,13 @@ def generate_scores(
 
 
 def inline(
-    span: Tuple[int, int], func: base.Function, arg: base.ASTNode
-) -> base.ASTNode:
+    span: Tuple[int, int],
+    func: lowered.Function,
+    args: Sequence[lowered.LoweredASTNode],
+) -> lowered.LoweredASTNode:
     """Merge a function and its argument to produce an expression."""
-    replacer = _Replacer(func.param, arg)
+    inlined = {param.value: arg for param, arg in zip(func.params, args)}
+    replacer = _Replacer(Scope.from_dict(inlined))
     result = replacer.run(func.body)
     result.span = span
     return result
