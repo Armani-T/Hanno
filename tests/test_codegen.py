8d37472fe339cf8238cc2e65cf609ebb9e594904
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-12-04 09:44:41 +0300

    Implement an encoding test for all the instruction types

--- tests/test_codegen.py
+++ tests/test_codegen.py
@@ -1,36 +1,109 @@
 # pylint: disable=C0116
-from pytest import mark
+from pytest import mark, param
 
-from context import codegen
+from context import codegen, lowered
 
 
 @mark.codegen
 @mark.parametrize(
-    "instruction,func_pool,string_pool,expected",
+    "instruction,expected_code,expected_func_pool,expected_string_pool",
     (
         (
             codegen.Instruction(codegen.OpCodes.LOAD_BOOL, (True,)),
-            [],
-            [],
             b"\x01\xff\x00\x00\x00\x00\x00\x00",
+            [],
+            [],
         ),
         (
             codegen.Instruction(codegen.OpCodes.LOAD_INT, (4200,)),
-            [],
-            [],
             b"\x03\x00\x00\x10\x68\x00\x00\x00",
+            [],
+            [],
+        ),
+        param(
+            codegen.Instruction(codegen.OpCodes.LOAD_FLOAT, (-2.718282,)),
+            b"\x02\xff\x01\x9e\xc6\xe4",
+            [],
+            [],
+            marks=mark.xfail,
+        ),
+        # TODO: Implement a way to handle overflow errors when enocding
+        #  floats.
+        (
+            codegen.Instruction(
+                codegen.OpCodes.LOAD_STRING, ("This is a jusτ a τεsτ string.",)
+            ),
+            b"\x04\x00\x00\x00\x00\x00\x00\x00",
+            [],
+            [b"This is a jus\xcf\x84 a \xcf\x84\xce\xb5s\xcf\x84 string."],
         ),
         (
-            codegen.Instruction(codegen.OpCodes.LOAD_FLOAT, (-2.718282,)),
+            codegen.Instruction(
+                codegen.OpCodes.LOAD_FUNC, ("This is a jusτ a τεsτ string.",)
+            ),
+            b"\x05\x00\x00\x00\x00\x00\x00\x00",
             [],
-            [],
-            b"\x02\xff\x01\x9e\xc6\xe4",
+            [b"This is a jus\xcf\x84 a \xcf\x84\xce\xb5s\xcf\x84 string."],
         ),
-    )
+        (
+            codegen.Instruction(codegen.OpCodes.BUILD_LIST, (200,)),
+            b"\x06\x00\x00\x00\xc8\x00\x00\x00",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.BUILD_TUPLE, (0,)),
+            b"\x07\x00\x00\x00\x00\x00\x00\x00",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.LOAD_NAME, (3, 26)),
+            b"\x08\x00\x00\x03\x00\x00\x00\x1a",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.STORE_NAME, (10, 8)),
+            b"\x09\x00\x0a\x00\x00\x00\x00\x08",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.CALL, (5,)),
+            b"\x0a\x00\x00\x00\x0a\x00\x00\x00",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.NATIVE, (lowered.OperationTypes.NEG,)),
+            b"\x0b\x09\x00\x00\x0a\x00\x00\x00",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.JUMP, (12,)),
+            b"\x0c\x00\x00\x00\x0b\x00\x00\x00",
+            [],
+            [],
+        ),
+        (
+            codegen.Instruction(codegen.OpCodes.BRANCH, (52,)),
+            b"\x0d\x00\x00\x00\x34\x00\x00\x00",
+            [],
+            [],
+        ),
+    ),
 )
-def test_encode(instruction, func_pool, string_pool, expected):
-    actual = codegen.encode(instruction.opcode, instruction.operands, func_pool, string_pool)
-    assert expected == actual
+def test_encode(instruction, expected_code, expected_func_pool, expected_string_pool):
+    actual_func_pool = []
+    actual_string_pool = []
+    actual_code = codegen.encode(
+        instruction.opcode, instruction.operands, actual_func_pool, actual_string_pool
+    )
+    assert expected_string_pool == actual_string_pool
+    assert expected_func_pool == actual_func_pool
+    assert expected_code == actual_code
 
 
 @mark.codegen
@@ -40,7 +113,7 @@ def test_encode(instruction, func_pool, string_pool, expected):
         (b"", b""),
         (b"\x00", b"\x00"),
         (b"aaaabbcccccdeeeeeeeeee", b"\x04a\x02b\x05c\x01d\x0ae"),
-    )
+    ),
 )
 def test_compress(source, expected):
     actual = codegen.compress(source)
@@ -77,7 +150,7 @@ def test_generate_lengths(source, expected):
         ),
         (
             ((265, b"x"), (16, b"y"), (782, b"z")),
-            b"\xffx\x0ax\x10y\xffz\xffz\xffz\x11z"
+            b"\xffx\x0ax\x10y\xffz\xffz\xffz\x11z",
         ),
     ),
 )
