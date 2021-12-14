f1edb8eb14a256b1c1114adae3957ab0cde0298b
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-12-14 21:29:29 +0300

    Update the tests to respect that the parser functions are private

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,10 +1,10 @@
 # pylint: disable=C0116, W0212
-from pytest import mark, raises
+from pytest import mark
 
-from context import base, errors, lex, parse
+from context import base, lex, parse
 
 
-def prepare(source: str, inference_on: bool = True) -> lex.TokenStream:
+def _prepare(source: str, inference_on: bool = True) -> lex.TokenStream:
     """
     Prepare a `TokenStream` for the lexer to use from a source string.
     """
@@ -12,83 +12,43 @@ def prepare(source: str, inference_on: bool = True) -> lex.TokenStream:
     return lex.TokenStream(inferer(lex.lex(source)))
 
 
+@mark.integration
 @mark.parsing
-def test_program_rule_when_token_stream_is_empty():
+def test_parser_on_empty_token_stream():
     stream = lex.TokenStream(iter(()))
-    result = parse._program(stream)
+    result = parse.parse(stream)
     assert isinstance(result, base.Vector)
     assert result.vec_type == base.VectorTypes.TUPLE
     assert not result.elements
 
 
-@mark.parsing
-@mark.parametrize(
-    "source,size,ends",
-    (
-        ("e, 3 * (10 ^ 8), epsilon, 344)", 4, (lex.TokenTypes.rparen,)),
-        ('"a", "b", "c", "d", "e",]', 5, (lex.TokenTypes.rbracket,)),
-        ('"a", "b", "c", "d", "e",)', 5, (lex.TokenTypes.rparen,)),
-    ),
-)
-def test_elements_rule(source, size, ends):
-    result = parse._elements(prepare(source, False), *ends)
-    assert len(result) == size
-    assert all((isinstance(elem, base.ASTNode) for elem in result))
-
-
+@mark.integration
 @mark.parsing
 @mark.parametrize(
     "source,expected",
     (
-        ("()", base.Vector((0, 2), base.VectorTypes.TUPLE, ())),
+        ("False", base.Scalar((0, 5), False)),
+        ("True", base.Scalar((0, 4), True)),
+        ("845.3142", base.Scalar((0, 7), 845.3142)),
+        ("124", base.Scalar((0, 3), 124)),
+        ('"αβγ"', base.Scalar((0, 3), "αβγ")),
+        ("()", base.Vector.unit((0, 2))),
         ("(3.142)", base.Scalar((1, 6), 3.142)),
         ("(3.142,)", base.Scalar((1, 6), 3.142)),
+        (
+            '["a", "b", "c",]',
+            base.Vector(
+                (0, 15),
+                base.VectorTypes.LIST,
+                [
+                    base.Scalar((2, 3), "a"),
+                    base.Scalar((2, 3), "b"),
+                    base.Scalar((2, 3), "c"),
+                ],
+            ),
+        ),
     ),
 )
-def test_tuple_rule(source, expected):
-    actual = parse._tuple(prepare(source, False))
+def test_parser(source, expected):
+    actual = parse.parse(_prepare(source, False))
     assert expected == actual
-
-
-@mark.parsing
-@mark.parametrize(
-    "source,expected",
-    (
-        ("False", False),
-        ("True", True),
-        ("845.3142", 845.3142),
-        ("124", 124),
-        ('"Hello, World!"', "Hello, World!"),
-        ('"αβγ"', "αβγ"),
-        ("some_var_name", "some_var_name"),
-        # NOTE: This builds a `Name`, NOT a `Scalar` with a `str` value
-    ),
-)
-def test_scalar_rule(source, expected):
-    actual = parse._scalar(prepare(source, False))
-    assert isinstance(actual, (base.Name, base.Scalar))
-    assert expected == actual.value
-
-
-@mark.parsing
-@mark.parametrize(
-    "source,expected_length",
-    (
-        ("x)", 1),
-        ("x,)", 1),
-        ("base, exp)", 2),
-        ("string, encoding, on_success, on_failure, ->", 4),
-    ),
-)
-def test_params_rule(source, expected_length):
-    actual = parse._params(prepare(source, False))
-    assert all(map(lambda arg: isinstance(arg, base.Name), actual))
-    assert expected_length > 0
-    assert expected_length == len(actual)
-
-
-@mark.parsing
-def test_params_fails_on_0_parameters():
-    with raises(errors.UnexpectedTokenError):
-        stream = lex.TokenStream(iter([lex.Token((0, 1), lex.TokenTypes.comma, None)]))
-        parse._params(stream)
