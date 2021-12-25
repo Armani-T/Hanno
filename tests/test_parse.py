a5a7dcf4c0bb617a2b5af7c0a9f99a75db5f7d79
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2021-12-25 18:32:06 +0300

    Fix a bug in the tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -140,7 +140,7 @@ def _prepare(source: str) -> lex.TokenStream:
                             base.FuncCall(
                                 span, base.Name(span, "func_1"), base.Scalar(span, 1)
                             ),
-                            base.Scalar(span, 1),
+                            base.Scalar(span, 2),
                         ),
                         base.FuncCall(
                             span,
