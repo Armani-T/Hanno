0ceb0b9c23f561323d119119bc8121e6a8b8d195
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2022-02-25 10:04:50 +0300

    Implement the lowered AST nodes instead of reusing the base nodes
    I decided to remove the `span` attribute because most program
    transformations will be done here so it will be very difficult to map
    the AST at this point back to the source code.

--- hasdrubal/asts/lowered.py
+++ hasdrubal/asts/lowered.py
@@ -1,16 +1,21 @@
 # pylint: disable=R0903, C0115, W0231
+from abc import ABC
 from enum import Enum, unique
-from typing import Sequence, Optional
+from typing import Any, MutableMapping, Sequence, Optional, Tuple, Union
 
-from . import base
+from .base import ASTNode, VectorTypes
 
-LoweredASTNode = base.ASTNode
-Block = base.Block
-Cond = base.Cond
-Define = base.Define
-Name = base.Name
-Scalar = base.Scalar
-Vector = base.Vector
+
+@unique
+class ValueTypes(Enum):
+    POINTER = 0
+    UNIT = 1
+    BOOL = 2
+    INT = 3
+    FLOAT = 4
+    STRING = 5
+    LIST = 6
+    TUPLE = 7
 
 
 @unique
@@ -38,11 +43,83 @@ class OperationTypes(Enum):
             return False
 
 
-class FuncCall(LoweredASTNode):
+class LoweredASTNode(ASTNode, ABC):
+    def __init__(self) -> None:
+        super().__init__((0, 0))
+        self._metadata: MutableMapping[str, Any] = {}
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(self, name)
+        except AttributeError:
+            result = self._metadata.get(name, None)
+            if result is None:
+                raise
+            return result
+
+
+class Block(LoweredASTNode):
+    def __init__(self, body: Sequence[LoweredASTNode]) -> None:
+        if not body:
+            raise ValueError("A block cannot have 0 expressions inside.")
+
+        super().__init__()
+        self.body: Sequence[LoweredASTNode] = body
+
+    def visit(self, visitor):
+        return visitor.visit_block(self)
+
+    def __eq__(self, other):
+        return isinstance(other, Block) and self.body == other.body
+
+    __hash__ = object.__hash__
+
+
+class Cond(LoweredASTNode):
     def __init__(
-        self, span: base.Span, func: LoweredASTNode, args: Sequence[LoweredASTNode]
+        self, pred: LoweredASTNode, cons: LoweredASTNode, else_: LoweredASTNode
     ) -> None:
-        super().__init__(span)
+        super().__init__()
+        self.pred: LoweredASTNode = pred
+        self.cons: LoweredASTNode = cons
+        self.else_: LoweredASTNode = else_
+
+    def visit(self, visitor):
+        return visitor.visit_cond(self)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Cond)
+            and self.pred == other.pred
+            and self.cons == other.cons
+            and self.else_ == other.else_
+        )
+
+    __hash__ = object.__hash__
+
+
+class Define(LoweredASTNode):
+    def __init__(self, target: "Name", value: LoweredASTNode) -> None:
+        super().__init__()
+        self.target: Name = target
+        self.value: LoweredASTNode = value
+
+    def visit(self, visitor):
+        return visitor.visit_define(self)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Define)
+            and self.target == other.target
+            and self.value == other.value
+        )
+
+    __hash__ = object.__hash__
+
+
+class FuncCall(LoweredASTNode):
+    def __init__(self, func: LoweredASTNode, args: Sequence[LoweredASTNode]) -> None:
+        super().__init__()
         self.func: LoweredASTNode = func
         self.args: Sequence[LoweredASTNode] = args
 
@@ -64,10 +141,10 @@ class FuncCall(LoweredASTNode):
 
 class Function(LoweredASTNode):
     def __init__(
-        self, span: base.Span, params: Sequence[Name], body: LoweredASTNode
+        self, params: Sequence[Tuple["Name", OperationTypes]], body: LoweredASTNode
     ) -> None:
-        super().__init__(span)
-        self.params: Sequence[Name] = params
+        super().__init__()
+        self.params: Sequence[Tuple[Name, OperationTypes]] = params
         self.body: LoweredASTNode = body
 
     def visit(self, visitor):
@@ -86,15 +163,28 @@ class Function(LoweredASTNode):
     __hash__ = object.__hash__
 
 
+class Name(LoweredASTNode):
+    def __init__(self, value: str) -> None:
+        super().__init__()
+        self.value: str = value
+
+    def visit(self, visitor) -> None:
+        return self.visif_name(self)
+
+    def __eq__(self, other):
+        return isinstance(other, Name) and self.value == other.value
+
+    __hash__ = object.__hash__
+
+
 class NativeOperation(LoweredASTNode):
     def __init__(
         self,
-        span: base.Span,
         operation: OperationTypes,
         left: LoweredASTNode,
         right: Optional[LoweredASTNode] = None,
     ) -> None:
-        super().__init__(span)
+        super().__init__()
         self.operation: OperationTypes = operation
         self.left: LoweredASTNode = left
         self.right: Optional[LoweredASTNode] = right
@@ -103,12 +193,46 @@ class NativeOperation(LoweredASTNode):
         return visitor.visit_native_operation(self)
 
     def __eq__(self, other) -> bool:
-        if isinstance(other, NativeOperation):
-            return (
-                self.operation == other.operation
-                and self.left == other.left
-                and self.right == other.right
-            )
-        return NotImplemented
+        return (
+            isinstance(other, NativeOperation)
+            and self.operation == other.operation
+            and self.left == other.left
+            and self.right == other.right
+        )
+
+    __hash__ = object.__hash__
+
+
+class Scalar(LoweredASTNode):
+    def __init__(self, value: Union[str, int, float, bool]) -> None:
+        super().__init__()
+        self.value: Union[str, int, float, bool] = value
+
+    def visit(self, visitor):
+        return visitor.visit_scalar(self)
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Scalar) and self.value == other.value
+
+    __hash__ = object.__hash__
+
+
+class Vector(LoweredASTNode):
+    def __init__(
+        self, vec_type: VectorTypes, elements: Sequence[LoweredASTNode]
+    ) -> None:
+        super().__init__()
+        self.vec_type: VectorTypes = vec_type
+        self.elements: Sequence[LoweredASTNode] = elements
+
+    def visit(self, visitor):
+        return visitor.visit_vector(self)
+
+    def __eq__(self, other) -> bool:
+        return (
+            isinstance(other, Vector)
+            and self.vec_type == other.vec_type
+            and tuple(self.elements) == tuple(other.elements)
+        )
 
     __hash__ = object.__hash__
