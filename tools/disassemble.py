7096c05c9302d983035bd541aa1933a0ed558596
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2022-02-27 12:42:51 +0300

    Refactor the 3 top-level functions to share more data

--- tools/disassemble.py
+++ tools/disassemble.py
@@ -1,24 +1,47 @@
 from pathlib import Path
-from sys import argv, exit
-from typing import Iterator, NoReturn
+from sys import argv, exit as sys_exit
+from typing import Any, Iterator, NoReturn, Sequence
 
 from context import codegen
 
 
 def decode_file(source: bytes) -> str:
-    ...
+    compression_flag, source = source[:2], source[2:]
+    source = decompress(source) if compression_flag == b"\xff\x00" else source
+    header_section, body_section = source.split(b"\r\n\r\n\r\n", 1)
+    headers = get_headers(header_section)
+    return (
+        headers,
+        get_instruction_stream(body_section),
+        get_func_pool(body_section, headers["func_pool_size"]),
+        get_string_pool(body_section, headers["string_pool_size"]),
+    )
 
 
-def explain_all(instructions: Iterator[codegen.Instruction]) -> str:
-    ...
+def explain_all(
+    headers: dict[str, Any],
+    instructions: Iterator[codegen.Instruction],
+    func_pool: tuple[int, bytes],
+    string_pool: Sequence[str],
+) -> str:
+    raise NotImplementedError
 
 
 def main() -> NoReturn:
-    if len(argv) > 1:
+    exit_code = 1
+    try:
         file = Path(argv[1])
-        bytecode = decode_file(file.read_bytes() if file.exists() else b"")
-        print(explain_all(bytecode))
-    exit(0)
+        headers, bytecode, func_pool, str_pool = decode_file(file.read_bytes())
+        explanation = explain_all(headers, bytecode, func_pool, str_pool)
+    except IndexError:
+        print("Please pass a Hasdrubal bytecode file as an argument.")
+    except FileNotFoundError:
+        print(f"File {argv[1]} not found.")
+    else:
+        exit_code = 0
+        print(explanation)
+    finally:
+        sys_exit(exit_code)
 
 
 if __name__ == "__main__":
