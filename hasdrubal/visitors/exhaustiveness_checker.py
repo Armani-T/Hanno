f192ec839078e7b372c01ef17508d3b5b00c7511
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2022-04-21 00:17:13 +0300

    Implement all the visitor methods for the exhaustiveness checker

--- hasdrubal/visitors/exhaustiveness_checker.py
+++ hasdrubal/visitors/exhaustiveness_checker.py
@@ -1,42 +1,55 @@
-from asts import typed
-from asts.base import Pattern
-from asts.types_ import Type
-from asts.visitors import TypedASTVisitor
+from asts import typed, visitors, types_ as types
+from errors import InexhaustivePatternError
 
 
-class ExhaustivenessChecker(TypedASTVisitor[None]):
+class ExhaustivenessChecker(visitors.TypedASTVisitor[None]):
     def visit_apply(self, node: typed.Apply) -> None:
-        ...
+        node.func.visit(self)
+        node.arg.visit(self)
 
     def visit_block(self, node: typed.Block) -> None:
-        ...
+        for expr in node.body:
+            expr.visit(self)
 
     def visit_cond(self, node: typed.Cond) -> None:
-        ...
+        node.pred.visit(self)
 
     def visit_define(self, node: typed.Define) -> None:
-        ...
+        if not is_exhaustive(node.target):
+            raise InexhaustivePatternError(node, node.target)
+        node.body.visit(self)
 
     def visit_function(self, node: typed.Function) -> None:
-        ...
+        if not is_exhaustive(node.param):
+            raise InexhaustivePatternError(node, node.param)
+        node.body.visit(self)
 
     def visit_list(self, node: typed.List) -> None:
-        ...
+        for elem in node.elements:
+            elem.visit(self)
 
     def visit_match(self, node: typed.Match) -> None:
-        ...
+        node.subject.visit(self)
+        exhaustive = False
+        for pattern, cons in node.cases:
+            exhaustive = exhaustive or is_exhaustive(pattern)
+            cons.visit(self)
+
+        if not exhaustive:
+            raise InexhaustivePatternError(node, node.cases[-1][0])
 
     def visit_pair(self, node: typed.Pair) -> None:
-        ...
+        node.first.visit(self)
+        node.second.visit(self)
 
     def visit_name(self, node: typed.Name) -> None:
-        ...
+        return
 
     def visit_scalar(self, node: typed.Scalar) -> None:
-        ...
+        return
 
-    def visit_type(self, node: Type) -> None:
-        ...
+    def visit_type(self, node: types.Type) -> None:
+        return
 
     def visit_unit(self, node: typed.Unit) -> None:
-        ...
+        return
