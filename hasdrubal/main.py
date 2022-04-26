f97b0b7c9dd62beb8a0c993d97bb586804f5d530
Armani Tallam <34724740+Armani-T@users.noreply.github.com>
2022-04-26 19:24:39 +0300

    Refactor `run_file`
    The `try` block inside it was catching developer errors that it
    wasn't supposed to. Some of these were coming from deeper inside the
    code, making debugging harder. Now the errors it can catch are specific
    to only that function.

--- hasdrubal/main.py
+++ hasdrubal/main.py
@@ -25,37 +25,33 @@ def run_file(config: ConfigData) -> int:
     """
     report, write = config.writers
     try:
+        if config.file is None:
+            logger.error("No file was passed in to be run.")
+            write(
+                "Please provide a file for the program to run."
+                f"\n\n{parser.format_usage()}\n"
+            )
+            return 64
         if config.file.is_dir():
             logger.fatal("A folder was passed into the program instead of a file.")
-            raise ValueError()
+            write(
+                report(
+                    errors.CMDError(errors.CMDErrorReasons.PATH_IS_FOLDER),
+                    "",
+                    str(config.file),
+                )
+            )
+            return 65
 
         source_bytes = config.file.resolve(strict=True).read_bytes()
         out_text = run_code(source_bytes, config)
         write(out_text)
         return 0
-    except AttributeError:
-        logger.error("No file was passed in to be run.")
-        write(
-            "Please provide a file for the program to run."
-            f"\n\n{parser.format_usage()}\n"
-        )
-        return 64
-    except ValueError:
-        write(
-            report(
-                errors.CMDError(errors.CMDErrorReasons.PATH_IS_FOLDER),
-                "",
-                str(config.file),
-            )
-        )
-        return 65
-    except (PermissionError, FileNotFoundError) as error:
-        new_error = errors.CMDError(
-            errors.CMDErrorReasons.NO_PERMISSION
-            if isinstance(error, PermissionError)
-            else errors.CMDErrorReasons.FILE_NOT_FOUND
-        )
-        write(report(new_error, "", str(config.file)))
+    except PermissionError:
+        write(report(errors.CMDError(errors.CMDErrorReasons.NO_PERMISSION), "", str(config.file)))
+        return 66
+    except FileNotFoundError:
+        write(report(errors.CMDError(errors.CMDErrorReasons.FILE_NOT_FOUND), "", str(config.file)))
         return 66
 
 
