c1ce7e9a622653827102717777a8e114bcc1bd96
Armani <armanitallam@gmail.com>
2022-12-31 23:38:09 +0300

    Remove a bug that decoded `LOAD_NAME` as 1 big number instead of 2

--- tools/disassemble.py
+++ tools/disassemble.py
@@ -1,13 +1,12 @@
+#! usr/bin/env python3
 from pathlib import Path
 from sys import argv, exit as sys_exit
-from typing import Any, Callable, Iterable, Iterator, NoReturn, Sequence, TypeVar
+from typing import Any, Iterable, Iterator, NoReturn, Sequence, TypeVar
 
 from context import codegen
 
 TV = TypeVar("TV", covariant=True)
 
-show_str_pool: Callable[[Iterable[str]], str]
-show_str_pool = lambda str_pool: "\n".join(map(repr, str_pool))
 split = lambda string, index: (string[:index], string[index:])
 
 
@@ -18,52 +17,41 @@ def get_int_value(sign: int, value: bytes) -> int:
     return sign_modifier * abs_value
 
 
-def get_float_value(sign: int, value: bytes) -> float:
-    base_sign = -1 if sign >= 0xF0 else +1
-    exp_sign = -1 if sign % 10 else +1
-    abs_base = float(int.from_bytes(value[:4], codegen.BYTE_ORDER, signed=False))
-    abs_exp = int.from_bytes(value[4:], codegen.BYTE_ORDER, signed=False)
-    return base_sign * (abs_base ** (exp_sign * abs_exp))
+def get_float_value(value: bytes) -> float:
+    base = float(int.from_bytes(value[:5], codegen.BYTE_ORDER, signed=True))
+    exponent = float(int.from_bytes(value[5:], codegen.BYTE_ORDER, signed=True))
+    return base**exponent
 
 
-def get_name_args(arg_space: bytes) -> tuple[int, int]:
-    depth_section, index_section = split(arg_space, 3)
-    return (
-        int.from_bytes(depth_section, codegen.BYTE_ORDER, signed=False),
-        int.from_bytes(index_section, codegen.BYTE_ORDER, signed=False),
-    )
-
-
-def get_op_args(opcode: int, arg_section: bytes) -> tuple[Any, ...]:
+def get_op_args(opcode: codegen.OpCodes, arg_section: bytes) -> tuple[Any, ...]:
     if opcode == codegen.OpCodes.LOAD_BOOL:
         return (arg_section[0] == 0xFF,)
     if opcode == codegen.OpCodes.LOAD_INT:
-        return (get_int_value(arg_section[0], arg_section[1:]),)
+        return (int.from_bytes(arg_section, codegen.BYTE_ORDER, signed=True),)
     if opcode == codegen.OpCodes.LOAD_FLOAT:
-        return (get_float_value(arg_section[0], arg_section[1:]),)
-    if opcode == codegen.OpCodes.BUILD_LIST:
-        return (int.from_bytes(arg_section[:4], codegen.BYTE_ORDER, signed=False),)
+        return (get_float_value(arg_section),)
+    if opcode == codegen.OpCodes.NATIVE:
+        return (arg_section[0],)
+    if opcode == codegen.OpCodes.LOAD_NAME:
+        return (
+            int.from_bytes(arg_section[:3], codegen.BYTE_ORDER, signed=False),
+            int.from_bytes(arg_section[3:], codegen.BYTE_ORDER, signed=False),
+        )
     if opcode in (
         codegen.OpCodes.APPLY,
-        codegen.OpCodes.NATIVE,
-        codegen.OpCodes.BUILD_TUPLE,
+        codegen.OpCodes.BUILD_PAIR,
+        codegen.OpCodes.LOAD_UNIT,
     ):
-        return (int.from_bytes(arg_section[:1], codegen.BYTE_ORDER, signed=False),)
-    if opcode in (codegen.OpCodes.LOAD_NAME, codegen.OpCodes.STORE_NAME):
-        return get_name_args(arg_section)
+        return ()
     return (int.from_bytes(arg_section, codegen.BYTE_ORDER, signed=False),)
 
 
 def get_instructions(source: bytes) -> Iterator[codegen.Instruction]:
-    current_index = 0
-    current_chunk = source[current_index : current_index + 8]
-    while current_chunk:
-        opcode, op_args = current_chunk[0], current_chunk[1:]
-        yield codegen.Instruction(
-            codegen.OpCodes(opcode),
-            get_op_args(opcode, op_args),
-        )
-        current_chunk = source[current_index : current_index + 8]
+    index = 0
+    while chunk := source[index : index + 8]:
+        opcode = codegen.OpCodes(chunk[0])
+        yield codegen.Instruction(opcode, get_op_args(opcode, chunk[1:]))
+        index += 8
 
 
 def read_pool(source: bytes) -> Sequence[bytes]:
@@ -77,32 +65,26 @@ def read_pool(source: bytes) -> Sequence[bytes]:
 
 
 def read_headers(source: bytes) -> tuple[dict[str, Any], bytes]:
-    func_pool_size = int.from_bytes(source[6:10], codegen.BYTE_ORDER, signed=False)
-    str_pool_size = int.from_bytes(source[13:17], codegen.BYTE_ORDER, signed=False)
+    func_pool_size = int.from_bytes(source[5:9], codegen.BYTE_ORDER, signed=False)
+    str_pool_size = int.from_bytes(source[11:15], codegen.BYTE_ORDER, signed=False)
     return {
-        "lib_mode": source[2] == 0xFF,
+        "lib_mode": bool(source[2]),
         "func_pool_size": func_pool_size,
         "str_pool_size": str_pool_size,
-        "stream_size": int.from_bytes(source[20:24], codegen.BYTE_ORDER, signed=False),
-        "encoding": source[27:37].rstrip(b"\x00").decode("ASCII"),
+        "stream_size": int.from_bytes(source[17:21], codegen.BYTE_ORDER, signed=False),
+        "encoding": source[23:35].rstrip(b"\x00").decode("ASCII"),
     }
-
-
-def remove_barrier(source: bytes) -> bytes:
-    if source.startswith(codegen.SECTION_SEP):
-        return source.lstrip(codegen.SECTION_SEP)
-    raise ValueError("Invalid separator found! This bytecode is in an invalid format.")
 
 
 def decompress(source: bytes) -> bytes:
     pieces = []
-    current_index = 0
-    current_chunk = source[current_index : current_index + 2]
-    while len(current_chunk) == 2:
-        pieces.append(bytes([current_chunk[1]] * current_chunk[0]))
-        current_index += 2
-        current_chunk = source[current_index : current_index + 2]
-    pieces.append(current_chunk)
+    index = 0
+    chunk = source[index : index + 2]
+    while len(chunk) == 2:
+        pieces.append(bytes([chunk[1]] * chunk[0]))
+        index += 2
+        chunk = source[index : index + 2]
+    pieces.append(chunk)
     return b"".join(pieces)
 
 
@@ -111,79 +93,80 @@ def decode_file(
 ) -> tuple[
     dict[str, Any], Sequence[bytes], Sequence[str], Iterator[codegen.Instruction]
 ]:
-    compression_flag, source = split(source, 2)
-    source = decompress(source) if compression_flag == b"\xff\x00" else source
-    header_source, source = split(source, 43)
+    compress_flag, source = split(source, 2)
+    source = decompress(source) if compress_flag == b"C\xFF" else source
+    header_source, source = split(source, 35)
+    if not source.startswith(b"\xFF" * 3):
+        raise ValueError("This bytecode is in an invalid format.")
+
     headers = read_headers(header_source)
-    func_source, source = split(remove_barrier(source), headers["func_pool_size"])
-    str_source, source = split(remove_barrier(source), headers["str_pool_size"])
+    func_source, source = split(source[3:], headers["func_pool_size"])
+    str_source, source = split(source, headers["str_pool_size"])
     return (
         headers,
         read_pool(func_source),
-        (string.decode(codegen.STRING_ENCODING) for string in read_pool(str_source)),
-        get_instructions(remove_barrier(source)),
+        [string.decode(codegen.STRING_ENCODING) for string in read_pool(str_source)],
+        get_instructions(source),
     )
 
 
-def show_instructions(instructions: Iterable[codegen.Instruction]) -> str:
-    def inner(opcode, operands):
-        if opcode in (codegen.OpCodes.LOAD_NAME, codegen.OpCodes.STORE_NAME):
-            return f"depth = {operands[0]}, index = {operands[1]}"
-        return repr(operands[0])
+def show_operand(opcode, operands):
+    if opcode == codegen.OpCodes.LOAD_NAME:
+        return f"( depth = {operands[0]}, index = {operands[1]} )"
+    if opcode in (
+        codegen.OpCodes.APPLY,
+        codegen.OpCodes.BUILD_PAIR,
+        codegen.OpCodes.LOAD_UNIT,
+    ):
+        return ""
+    return f"( {repr(operands[0])} )"
 
-    return "\n".join(
-        f"{instr.opcode.name}( {inner(instr.opcode, instr.operands)} )"
+
+def show_instructions(
+    instructions: Iterable[codegen.Instruction], indent: int = 0
+) -> str:
+    return ("\n" + "\t" * indent).join(
+        f"{instr.opcode.name}{show_operand(instr.opcode, instr.operands)}"
         for instr in instructions
     )
 
 
 def show_func_pool(func_pool: Iterable[bytes]) -> str:
-    functions = []
-    for index, bytecode in enumerate(func_pool):
-        body = show_instructions(get_instructions(bytecode))
-        full = f"Function object #{index}:\n{body}"
-        full = full.replace("\n", "\n    ")
-        functions.append(full)
-    return "\n\n".join(functions)
+    show_body = lambda code: show_instructions(get_instructions(code), indent=1)
+    return "\n".join(
+        f"Function object #{index}:\n\t{show_body(bytecode)}"
+        for index, bytecode in enumerate(func_pool)
+    )
 
 
 def show_headers(headers: dict[str, Any]) -> str:
-    return "\n".join(
-        (
-            "Headers:",
-            f"Function Pool Size (bytes): {headers['func_pool_size']}",
-            f"String Encoding:            {headers['encoding']}",
-            f"String Pool Size (bytes):   {headers['func_pool_size']}",
-            f"Instruction Stream Size:    {headers['stream_size']}",
-            f"Library Mode:               {'ON' if headers['lib_mode'] else 'OFF'}",
-        )
+    return (
+        "Headers\n"
+        "-------\n"
+        f"Library Mode:            {'ON' if headers['lib_mode'] else 'OFF'}\n"
+        f"String Encoding:         {headers['encoding']}\n"
+        f"Function Pool Size:      {headers['func_pool_size']} bytes\n"
+        f"String Pool Size:        {headers['func_pool_size']} bytes\n"
+        f"Instruction Stream Size: {headers['stream_size']} bytes"
     )
 
 
 def main() -> NoReturn:
-    exit_code = 1
     try:
+        if len(argv) == 1:
+            print("Please pass in a Hanno bytecode file.")
+            sys_exit(1)
+
         file_contents = Path(argv[1]).read_bytes()
-        headers, string_pool, func_pool, instructions = decode_file(file_contents)
-        explanation = "\n\n\n".join(
-            (
-                show_headers(headers),
-                show_str_pool(string_pool),
-                show_func_pool(func_pool),
-                "" if headers["lib_mode"] else show_instructions(instructions),
-            )
-        )
-    except IndexError:
-        print("Please pass a Hasdrubal bytecode file as an argument.")
+        headers, func_pool, string_pool, instructions = decode_file(file_contents)
+        print(show_headers(headers), "\n")
+        print(show_func_pool(func_pool), "\n")
+        if not headers["lib_mode"]:
+            print(show_instructions(instructions))
+        sys_exit(0)
     except FileNotFoundError:
         print(f'"{argv[1]}" has not been found.')
-    except ValueError as error:
-        print(error.args[0])
-    else:
-        print(explanation)
-        exit_code = 0
-    finally:
-        sys_exit(exit_code)
+        sys_exit(1)
 
 
 if __name__ == "__main__":
