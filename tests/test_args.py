3c2f1c3fc2a21a0f21cebde0aff39cd521af2511
Armani Tallam <armanitallam@gmail.com>
2026-05-07 13:23:32 +0300

    Take test coverage for the `args` module to 100%

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,8 +1,10 @@
 from codecs import lookup
+from pathlib import Path
+from sys import stderr, stdout
 
-from pytest import mark
+from pytest import mark, raises
 
-from context import args
+from context import args, errors
 
 
 class FakeNamespace:
@@ -14,7 +16,7 @@ class FakeNamespace:
 
 @mark.cmd
 @mark.parametrize(
-    "cmd_args,expected_",
+    "cmd_args,expected",
     (
         ((), {}),
         (("-?",), {"show_help": True}),
@@ -24,11 +26,11 @@ class FakeNamespace:
         ),
     ),
 )
-def test_build_config(cmd_args, expected_):
+def test_build_config(cmd_args, expected):
     namespace = FakeNamespace()
     args.parser.parse_args(args=cmd_args, namespace=namespace)
     config = args.build_config(namespace)
-    expected = args.DEFAULT_CONFIG | expected_
+    expected = args.DEFAULT_CONFIG | expected
 
     assert lookup(config.encoding) == lookup(expected.encoding)
     assert expected.file == config.file
@@ -40,3 +42,91 @@ def test_build_config(cmd_args, expected_):
     assert expected.sort_defs == config.sort_defs
     assert len(config.writers) == 2
     assert all(map(callable, config.writers))
+
+
+@mark.cmd
+@mark.parametrize(
+    "data",
+    (
+        {},
+        {
+            "encoding": lookup("Latin-1"),
+            "show_types": True,
+            "sort_defs": False,
+            "show_version": True,
+        },
+        args.DEFAULT_CONFIG,
+        args.ConfigData(
+            file=None,
+            encoding=lookup("ASCII"),
+            compress=True,
+            expansion_level=10,
+            out_file="stderr",
+            show_ast=True,
+            show_help=True,
+            show_version=False,
+            show_tokens=True,
+            show_types=False,
+            sort_defs=True,
+            writers=(),
+        ),
+    ),
+)
+def test_config_data_or(data):
+    config = args.DEFAULT_CONFIG | data
+    keys = (
+        "file",
+        "encoding",
+        "compress",
+        "expansion_level",
+        "out_file",
+        "show_ast",
+        "show_help",
+        "show_version",
+        "show_tokens",
+        "show_types",
+        "sort_defs",
+        "writers",
+    )
+    for key in keys:
+        if key in data:
+            assert config[key] == data[key]
+        else:
+            assert config[key] == args.DEFAULT_CONFIG[key]
+
+
+@mark.cmd
+def test_config_data_or_raises():
+    with raises(TypeError):
+        args.DEFAULT_CONFIG | []
+
+
+@mark.cmd
+def test_config_data_contains_when_false():
+    assert "zzzzz" not in args.DEFAULT_CONFIG
+
+
+@mark.cmd
+def test_get_writer():
+    assert stdout.write == args.get_writer(None)
+    assert stdout.write == args.get_writer("stdout")
+    assert stderr.write == args.get_writer("stderr")
+
+
+@mark.cmd
+def test_get_writer_raises_1(monkeypatch):
+    monkeypatch.setattr(Path, "touch", lambda _: None)
+    with raises(errors.CMDError):
+        path = Path(__file__).parent / "does-not-exist.txt"
+        result = args.get_writer(str(path))
+
+
+@mark.cmd
+def test_get_writer_raises_2(monkeypatch):
+    def throw_error(_):
+        raise PermissionError
+
+    monkeypatch.setattr(Path, "touch", throw_error)
+    with raises(errors.CMDError):
+        path = Path(__file__).parent / "does-not-exist.txt"
+        result = args.get_writer(str(path))
